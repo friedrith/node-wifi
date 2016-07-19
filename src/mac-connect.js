b5bdf01dfd9c55d96d72e8ffd166b1c35818bfcf
thomas Pouget-abadie <toefjerf@gmail.com>
2016-07-19 13:35:03 +0200

    updated

--- src/mac-connect.js
+++ src/mac-connect.js
@@ -3,13 +3,14 @@ var exec = require('child_process').exec;
 function connectToWifi(config) {
 
     return function(ap, callback) {
+
+	var commandStr = "networksetup -setairportnetwork ";
 	
 	if (config.iface) {
-	    var commandStr = "networksetup -setairportnetwork " +
-		config.iface.toString() + " " + ap.ssid + " " + ap.password;
-	} else {
-	    var commandStr = "networksetup -setairportnetwork en0 " +
+	    commandStr += config.iface.toString() + " " +
 		ap.ssid + " " + ap.password;
+	} else {
+	    commandStr += "en0 " + ap.ssid + " " + ap.password;
 	}
 	
 	exec(commandStr, function(err, resp) {
