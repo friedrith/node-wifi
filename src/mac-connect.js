cb17adffcb1bef8c52264ff930ff9a329525e793
thomas Pouget-abadie <toefjerf@gmail.com>
2016-07-26 16:21:56 +0200

    added '' to ssid and password fields for mac

--- src/mac-connect.js
+++ src/mac-connect.js
@@ -2,17 +2,17 @@ var exec = require('child_process').exec;
 var util = require('util');
 
 function connectToWifi(config) {
-
+    
     return function(ap, callback) {
 
 	var new_env = util._extend(process.env, { LANG: "en"});
 	var commandStr = "networksetup -setairportnetwork ";
 	
 	if (config.iface) {
-	    commandStr += config.iface.toString() + " " +
-		ap.ssid + " " + ap.password;
+	    commandStr += config.iface.toString() + " '" +
+		ap.ssid + "' '" + ap.password + "'";
 	} else {
-	    commandStr += "en0 " + ap.ssid + " " + ap.password;
+	    commandStr += "'en0' '" + ap.ssid + "' '" + ap.password;
 	}
 	
 	exec(commandStr, new_env, function(err, resp) {
