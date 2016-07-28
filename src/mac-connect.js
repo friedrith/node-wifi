5b8dcad344284a38021a06a9fada4ba9f75b35eb
thomas Pouget-abadie <toefjerf@gmail.com>
2016-07-28 13:41:42 +0200

    fixed bug concerning missing apostrophe

--- src/mac-connect.js
+++ src/mac-connect.js
@@ -6,14 +6,14 @@ function connectToWifi(config) {
     return function(ap, callback) {
 
 	var new_env = util._extend(process.env, { LANG: "en"});
+	var iface = 'en0';
 	var commandStr = "networksetup -setairportnetwork ";
 	
 	if (config.iface) {
-	    commandStr += config.iface.toString() + " '" +
-		ap.ssid + "' '" + ap.password + "'";
-	} else {
-	    commandStr += "'en0' '" + ap.ssid + "' '" + ap.password;
+	    iface = config.iface.toString();
 	}
+	commandStr = commandStr + "'" + iface + "'" + " " + "'" + ap.ssid + "'" + " " + "'" + ap.password + "'";
+	console.log(commandStr);
 	
 	exec(commandStr, new_env, function(err, resp) {
 	    
