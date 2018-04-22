d9c7e0378351aba636a74538a476d71e69c82232
Fractal <fractalzombie@gmail.com>
2018-04-23 00:57:51 +0300

    Update env.js

--- src/env.js
+++ src/env.js
@@ -1,3 +1,3 @@
 var util = require('util');
 
-module.exports = util._extend(process.env, { LANG: "en", LC_ALL: "en", LC_MESSAGES: "en"});
+module.exports = util._extend(process.env, { LANG: "en_US.UTF-8", LC_ALL: "en_US.UTF-8", LC_MESSAGES: "en_US.UTF-8"});
