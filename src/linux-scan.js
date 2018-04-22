2a5b119f9de0573d9f71faefa2dfac3705352398
Fractal <fractalzombie@gmail.com>
2018-04-22 23:58:05 +0300

    Update linux-scan.js

--- src/linux-scan.js
+++ src/linux-scan.js
@@ -17,7 +17,7 @@ function scanWifi(config, callback) {
       var lines = scanResults.split('\n');
       var networks = [];
       for (var i = 0 ; i < lines.length ; i++) {
-        if (lines[i] != '') {
+        if (lines[i] != '' && lines[i] != 'undefined') {
           var fields = lines[i].replace(/\\\:/g, '&&').split(':');
           networks.push({
             ssid: fields[1].replace(/\&\&/g, ':'),
