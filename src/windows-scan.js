723c016323b99bf833971be929fec40341a9c08b
Kimmo Saari <kirbo@kirbo-designs.com>
2020-02-06 04:06:50 +0200

    fix: windows fixes

--- src/windows-scan.js
+++ src/windows-scan.js
@@ -14,50 +14,45 @@ function scanWifi(config, callback) {
           return;
         }
 
-        scanResults = scanResults
-          .toString('utf8')
-          .split('\r')
-          .join('')
-          .split('\n')
-          .slice(5, scanResults.length);
+        const ssids = scanResults.toString('utf8').split(/^SSID .* : /gmi);
+        ssids.shift();
 
-        var numNetworks = -1;
-        var currentLine = 0;
-        var networkTmp;
-        var networksTmp = [];
-        var network;
-        var networks = [];
-        var i;
+        const networks = ssids.map(ssidText => {
+          const bssidsText = ssidText.toString('utf8').replace(/\r/gi, '').split(/^\s+BSSID .* : /gmi);
 
-        const bssids = [];
+          const ssid = bssidsText[0].split('\n')[0];
+          const network = bssidsText[0].replace(/\r/gi, '').split('\n');
+          const authentication = network[2].match(/.*?:\s(.*)/)[1];
+          const encryption = network[3].match(/.*?:\s(.*)/)[1];
+          //const networkType = network[1].match(/.*?:\s(.*)/)[1];
 
-        for (i = 0; i < scanResults.length; i++) {
-          if (scanResults[i] === '') {
-            numNetworks++;
-            networkTmp = scanResults.slice(currentLine, i);
-            networksTmp.push(networkTmp);
-            currentLine = i + 1;
-          }
-        }
+          bssidsText.shift();
+          const bssids = bssidsText.map(bssidText => {
+            const bssid = bssidText.split('\n');
+            const mac = bssid[0];
+            const signal = bssid[1].match(/.*?:\s(.*)/)[1];
+            const channel = bssid[3].match(/.*?:\s(.*)/)[1];
+            //const radioType = bssid[2].match(/.*?:\s(.*)/)[1];
+            //const basicRates = bssid[4].match(/.*?:\s(.*)/)[1];
+            //const otherRates = bssid[5].match(/.*?:\s(.*)/)[1];
 
-        for (i = 0; i < networksTmp.length; i++) {
-          let thisNetwork = networksTmp[i];
-          if (thisNetwork.length > 0) {
-            const splits = thisNetwork.join('\n').toString().split(/BSSID/i);
-            if (splits.length > 2) {
-              for (a = 2; a < splits.length; a++) {
-                networks.push(parseBssid(thisNetwork, splits[a].split('\n')));
-              }
-            } else {
-              bssids.push(thisNetwork);
-            }
-          }
-        }
+            return {
+              ssid,
+              mac,
+              bssid: mac,
+              channel: Number(channel),
+              frequency: channel ? parseInt(networkUtils.frequencyFromChannel(channel)) : 0,
+              signal_level: signal ? networkUtils.dBFromQuality(signal) : Number.MIN_VALUE,
+              quality: signal ? parseFloat(signal) : 0,
+              security: authentication,
+              security_flags: encryption,
+              mode: 'Unknown',
+            };
+          })
 
-        for (i = 0; i < bssids.length; i++) {
-          network = parse(networksTmp[i]);
-          networks.push(network);
-        }
+          return bssids;
+        })
+        .reduce((current, array) => [...array, ...current], [])
 
         callback && callback(null, networks);
       }
@@ -65,50 +60,6 @@ function scanWifi(config, callback) {
   } catch (e) {
     callback && callback(e);
   }
-}
-
-function parse(networkTmp) {
-  var network = {};
-
-  network.mac = networkTmp[4] ? networkTmp[4].match(/.*?:\s(.*)/)[1] : '';
-  network.bssid = network.mac;
-  network.ssid = networkTmp[0] ? networkTmp[0].match(/.*?:\s(.*)/)[1] : '';
-  network.channel = networkTmp[7]
-    ? parseInt(networkTmp[7].match(/.*?:\s(.*)/)[1])
-    : -1;
-  network.frequency = network.channel
-    ? parseInt(networkUtils.frequencyFromChannel(network.channel))
-    : 0;
-  network.signal_level = networkTmp[5]
-    ? networkUtils.dBFromQuality(networkTmp[5].match(/.*?:\s(.*)/)[1])
-    : Number.MIN_VALUE;
-  network.quality = networkTmp[5]
-    ? parseFloat(networkTmp[5].match(/.*?:\s(.*)/)[1])
-    : 0;
-  network.security = networkTmp[2] ? networkTmp[2].match(/.*?:\s(.*)/)[1] : '';
-  network.security_flags = networkTmp[3]
-    ? networkTmp[3].match(/.*?:\s(.*)/)[1]
-    : '';
-  network.mode = 'Unknown';
-
-  return network;
-}
-
-function parseBssid(networkTmp, bssid) {
-  var network = {};
-
-  network.mac = bssid[0].match(/.*?:\s(.*)/)[1];
-  network.bssid = network.mac;
-  network.ssid = networkTmp[0].match(/.*?:\s(.*)/)[1];
-  network.channel = parseInt(bssid[3].match(/.*?:\s(.*)/)[1]);
-  network.frequency = parseInt(networkUtils.frequencyFromChannel(network.channel));
-  network.signal_level = networkUtils.dBFromQuality(bssid[1].match(/.*?:\s(.*)/)[1]);
-  network.quality = parseFloat(bssid[4].match(/.*?:\s(.*)/)[1]);
-  network.security = networkTmp[2].match(/.*?:\s(.*)/)[1];
-  network.security_flags = networkTmp[3].match(/.*?:\s(.*)/)[1];
-  network.mode = 'Unknown';
-
-  return network;
 }
 
 module.exports = function (config) {
