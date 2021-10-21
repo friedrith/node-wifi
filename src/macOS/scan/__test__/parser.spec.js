81d9833616c937b2f5cf71bc7fa5b99d464d8458
Thibault Friedrich <thibault.friedrich@gmail.com>
2021-10-21 09:35:53 -0400

    --wip-- [skip ci]

--- src/macOS/scan/__test__/parser.spec.js
+++ src/macOS/scan/__test__/parser.spec.js
@@ -58,6 +58,59 @@ describe('parse macOS scan output', () => {
     ]);
   });
 
+  it('should return wifi networks on macOS monterey', async () => {
+    const output = await unlog(log('scan-monterey.log'));
+
+    const networks = parse(output);
+
+    expect(networks).toEqual([
+      {
+        mac: 'f6:d1:6d:b3:d2:88',
+        bssid: 'f6:d1:6d:b3:d2:88',
+        ssid: 'foo-bar',
+        channel: 6,
+        frequency: 2437,
+        quality: 28,
+        signal_level: '-86',
+        security: 'WPA2',
+        security_flags: ['(PSK/AES/AES)']
+      },
+      {
+        mac: 'c1:ee:0e:e4:fa:1f',
+        bssid: 'c1:ee:0e:e4:fa:1f',
+        ssid: 'VIRGO1732',
+        channel: 36,
+        frequency: 5180,
+        quality: 28,
+        signal_level: '-86',
+        security: 'WPA2',
+        security_flags: ['(PSK/AES/AES)']
+      },
+      {
+        mac: '34:e8:94:1e:fc:5c',
+        bssid: '34:e8:94:1e:fc:5c',
+        ssid: 'NERMNET',
+        channel: 1,
+        frequency: 2412,
+        quality: 90,
+        signal_level: '-55',
+        security: 'WPA WPA2',
+        security_flags: ['(PSK/AES,TKIP/TKIP)', '(PSK/AES,TKIP/TKIP)']
+      },
+      {
+        mac: '45:ff:7b:43:b5:26',
+        bssid: '45:ff:7b:43:b5:26',
+        ssid: 'NERMNET',
+        channel: 149,
+        frequency: 5745,
+        quality: 72,
+        signal_level: '-64',
+        security: 'WPA WPA2',
+        security_flags: ['(PSK/TKIP,AES/TKIP)', '(PSK/TKIP,AES/TKIP)']
+      }
+    ]);
+  });
+
   it('should return wifi networks with shifted lines', async () => {
     const output = await unlog(log('scan-shifted.log'));
 
