14fe263e07b9b102327617b3bf67a7e4afeadddb
Max Hellwig <mail@maxhellwig.de>
2022-09-16 11:28:01 +0200

    feat: Support Wifi with hidden SSID on Windows
    Related to #22
    https://github.com/friedrith/node-wifi/issues/22

--- src/windows-connect.js
+++ src/windows-connect.js
@@ -23,58 +23,64 @@ function execCommand(cmd, params) {
 }
 
 function connectToWifi(config, ap, callback) {
-  scan(config)()
-    .then(networks => {
-      const selectedAp = networks.find(network => {
+  let selectedAp = null;
+  let profile;
+  if (ap.hiddenWifi) {
+    selectedAp = {
+      ssid: ap.ssid,
+      security: ['WPA2'],
+    };
+
+    profile = Promise.resolve(selectedAp);
+  } else {
+    profile = scan(config)().then(networks => {
+      selectedAp = networks.find(network => {
         return network.ssid === ap.ssid;
       });
+    });
+  }
 
-      if (selectedAp === undefined) {
-        throw 'SSID not found';
-      }
+  if (selectedAp === undefined) {
+    throw 'SSID not found';
+  }
+  fs.writeFileSync(
+      profileFilename,
+      win32WirelessProfileBuilder(selectedAp, ap.password, ap.hiddenWifi),
+  );
 
-      fs.writeFileSync(
-        profileFilename,
-        win32WirelessProfileBuilder(selectedAp, ap.password)
-      );
-    })
-    .then(() => {
-      return execCommand('netsh', [
-        'wlan',
-        'add',
-        'profile',
-        `filename=${profileFilename}`
-      ]);
-    })
-    .then(() => {
-      const cmd = 'netsh';
-      const params = [
-        'wlan',
-        'connect',
-        `ssid="${ap.ssid}"`,
-        `name="${ap.ssid}"`
-      ];
-      if (config.iface) {
-        params.push(`interface="${config.iface}"`);
-      }
-      return execCommand(cmd, params);
-    })
-    .then(() => {
-      return execCommand(`del ${profileFilename}`);
-    })
-    .then(() => {
-      callback && callback();
-    })
-    .catch(err => {
-      execFile(
+  profile.then(() => {
+    return execCommand('netsh', [
+      'wlan',
+      'add',
+      'profile',
+      `filename=${profileFilename}`,
+    ]);
+  }).then(() => {
+    const cmd = 'netsh';
+    const params = [
+      'wlan',
+      'connect',
+      `ssid="${ap.ssid}"`,
+      `name="${ap.ssid}"`,
+    ];
+    if (config.iface) {
+      params.push(`interface="${config.iface}"`);
+    }
+    return execCommand(cmd, params);
+  }).then(() => {
+    return execCommand(`del ${profileFilename}`);
+  }).then(() => {
+    callback && callback();
+  }).catch(err => {
+    execFile(
         'netsh',
         ['wlan', 'delete', `profile "${ap.ssid}"`],
-        { env },
+        {env},
         () => {
           callback && callback(err);
         }
-      );
-    });
+    );
+  });
 }
 
 function getHexSsid(plainTextSsid) {
@@ -93,12 +99,21 @@ function getHexSsid(plainTextSsid) {
   return hex;
 }
 
-function win32WirelessProfileBuilder(selectedAp, key) {
-  let profile_content = `<?xml version="1.0"?> <WLANProfile xmlns="http://www.microsoft.com/networking/WLAN/profile/v1"> <name>${
-    selectedAp.ssid
-  }</name> <SSIDConfig> <SSID> <hex>${getHexSsid(
-    selectedAp.ssid
-  )}</hex> <name>${selectedAp.ssid}</name> </SSID> </SSIDConfig>`;
+function win32WirelessProfileBuilder(selectedAp, key, hiddenWifi = false) {
+  let profile_content = `
+  <?xml version="1.0"?> <WLANProfile xmlns="http://www.microsoft.com/networking/WLAN/profile/v1">
+    <name>${ selectedAp.ssid }</name>
+    <SSIDConfig>
+     <SSID>
+       <hex>
+         ${getHexSsid(selectedAp.ssid,)}
+       </hex>
+       <name>${selectedAp.ssid}</name> 
+     </SSID>
+     <nonBroadcast>
+       ${hiddenWifi ?'true' : 'false'}
+     </nonBroadcast>
+    </SSIDConfig>`;
 
   if (selectedAp.security.includes('WPA2')) {
     profile_content += `<connectionType>ESS</connectionType> <connectionMode>auto</connectionMode> <autoSwitch>true</autoSwitch> <MSM> <security> <authEncryption> <authentication>WPA2PSK</authentication> <encryption>AES</encryption> <useOneX>false</useOneX> </authEncryption> <sharedKey> <keyType>passPhrase</keyType> <protected>false</protected> <keyMaterial>${key}</keyMaterial> </sharedKey> </security> </MSM>`;
